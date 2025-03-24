acf30fc71ca3d1b77d4b24b1c753f883d90f0982
neerp03 <157861342+neerp03@users.noreply.github.com>
2025-03-24 19:27:35 -0400

    Create Main.java

---
+++ Char Occurrence/Main.java
@@ -0,0 +1,30 @@
+package idk2dsa;
+
+import java.util.Scanner;
+
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String s = input.nextLine();
+        System.out.print("Enter a character: ");
+        char ch = input.next().charAt(0);
+
+        System.out.println("The number of occurrences of \'" + ch +"\' in \"" + s + "\" is " + count(s,ch));
+    }
+
+    public static int count(String str, char a) {
+
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+
+            if (str.charAt(i) == a) count++;
+        }
+
+        return count;
+    }
+}
